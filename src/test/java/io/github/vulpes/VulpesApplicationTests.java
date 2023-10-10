6a36fe9e3921e9afd9e6932b7b2e71f50fbe299a
Kaique de Miranda <kbdemiranda@hotmail.com>
2023-10-09 22:13:40 -0300

    Iniciando o desenvolvimento

---
+++ src/test/java/io/github/vulpes/VulpesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vulpes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulpesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
