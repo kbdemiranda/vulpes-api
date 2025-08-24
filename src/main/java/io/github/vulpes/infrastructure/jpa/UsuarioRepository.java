98881b09cc7b66f0d2b80ef76cabc8f8291b36a3
Kaique de Miranda <kbdemiranda@hotmail.com>
2025-08-24 20:50:21 -0300

    refactor: Add `@Transactional` and `clearAutomatically` to `deleteUsuario` to improve consistency and ensure transaction management

--- src/main/java/io/github/vulpes/infrastructure/jpa/UsuarioRepository.java
+++ src/main/java/io/github/vulpes/infrastructure/jpa/UsuarioRepository.java
@@ -9,11 +9,15 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     Optional<Usuario> findByEmail(String email);
 
-    @Query("update Usuario u set u.excluidoEm = now() where u.id = :id")
-    @Modifying
-    void deleteUsuario(@Param("id") Long id);
+    @Query("update Usuario u set u.excluidoEm = CURRENT_TIMESTAMP where u.id = :id")
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    int deleteUsuario(@Param("id") Long id);
 }
